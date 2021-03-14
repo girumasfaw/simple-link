6edef66b5dfa8595460cb832ef21969f83d84b27
Girum Asfaw <33421525+girumasfaw@users.noreply.github.com>
2021-03-14 16:51:22 +0100

    remove dead code

--- src/context/reducer.ts
+++ src/context/reducer.ts
@@ -5,15 +5,6 @@ import { ILink } from "../models/link.interface";
 
 export const reducer = (state: {trackHeight: number, timelineWidth: number, blocks: IBlock[], links: ILink[]}, action: IBlockAction) => {
     switch (action.type) {
-      // case "ADD_ARTICLE":
-      //   return [
-      //     ...state,
-      //     {
-      //       id: Math.random(), // not really unique but it's just an example
-      //       title: action.article.title,
-      //       body: action.article.body
-      //     }
-      //   ];
       default:
         return state;
     }
